426613f6d88d779247fde438856eb3e149d2920a
himanshuy <himanshuy@himanshuy-ubuntu.(none)>
2025-11-24 15:55:43 +0530

    custom txt....

--- axe-custom-rules.js
+++ axe-custom-rules.js
@@ -1,34 +1,13 @@
-// axe-custom-rules.js
 module.exports = {
     'min-font-size': {
-        meta: {
-            type: 'accessibility',
-            docs: {
-                description: 'Ensure text has a minimum font size of 14px',
-            },
-            messages: {
-                fail: 'Font size too small (<14px)',
-            },
-        },
+        meta: { type: 'accessibility' },
         create: (context) => ({
             JSXOpeningElement(node) {
-                // Check if style attribute exists
-                const styleAttr = node.attributes.find(
-                    attr => attr.name && attr.name.name === 'style'
-                );
-
-                if (styleAttr && styleAttr.value && styleAttr.value.expression?.properties) {
-                    const fontProp = styleAttr.value.expression.properties.find(
-                        p => p.key.name === 'fontSize'
-                    );
-                    if (fontProp) {
-                        const fontValue = parseFloat(fontProp.value.value);
-                        if (fontValue < 14) {
-                            context.report({
-                                node,
-                                messageId: 'fail',
-                            });
-                        }
+                const styleAttr = node.attributes.find(attr => attr.name?.name === 'style');
+                if (styleAttr?.value?.expression?.properties) {
+                    const fontProp = styleAttr.value.expression.properties.find(p => p.key.name === 'fontSize');
+                    if (fontProp && parseFloat(fontProp.value.value) < 14) {
+                        context.report({ node, message: 'Font size too small (<14px)' });
                     }
                 }
             },
